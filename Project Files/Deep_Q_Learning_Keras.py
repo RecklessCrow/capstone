3ce95fc69963d96cf56eea9ae74e27fc0bacf097
CJ <jmcc2646@gmail.com>
2020-04-25 13:00:58 -0400

    Implemented PPO algorithm, improved readability, testing DQL using a different framework.

--- Project Files/Deep_Q_Learning_Keras.py
+++ Project Files/Deep_Q_Learning_Keras.py
@@ -1,38 +1,41 @@
-import cv2
-from keras.models import Sequential
-from keras.layers import ConvLSTM2D, Conv2D, Dense, Flatten, Permute
-from keras.optimizers import Adam, nadam
-
-from rl.agents.dqn import DQNAgent
-from rl.agents.ddpg import DDPGAgent
-from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy, BoltzmannQPolicy, MaxBoltzmannQPolicy, \
-    BoltzmannGumbelQPolicy, GreedyQPolicy
-from rl.memory import SequentialMemory
-from rl.core import Processor
-
-from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import datetime
+from rl.callbacks import FileLogger
+from PIL import Image, ImageOps
+from keras.layers import Conv2D, Dense, Flatten, Permute
+from keras.models import Sequential
+from keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.core import Processor
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 
 # Constants
-TARGET_IMAGE_SHAPE = (128, 128)  # Size we want our image to be for input into CNN in (w, h) for B&W
+PIXELS = 128
+TARGET_IMAGE_SHAPE = (PIXELS, PIXELS)  # Size we want our image to be for input into CNN in (w, h) for B&W image
 
-ALPHA = 0.00025  # Learning rate
+ALPHA = 0.05  # Learning rate
 GAMMA = 0.99  # Discount rate
-EPSILON_MAX = 1.0  # Probability of a random action
-EPSILON_MIN = 0.1
+EPSILON_MAX = 1.0  # Start value for eps during training
+EPSILON_MIN = 0.05  # Lowest value for eps during training
 EPSILON_TEST = 0.25
 
+STEPS = 2500000  # Number of actions the NN will take in training
 MAX_EXPERIENCES = 1000000  # Max size of replay buffer
-EXAMPLE_PERIOD  = MAX_EXPERIENCES / 10  # Number actions before NN training kicks in
-TARGET_UPDATE   = 25000  # Number of actions in an update set
-WINDOW_LENGTH   = 5  # Number of frames observable in an input
+EXAMPLE_PERIOD = 10000  # Number actions before NN training kicks in
+TARGET_UPDATE = 10000  # Number of actions in an update set
+WINDOW_LENGTH = 5  # Number of frames observable in an input
+DENSE = 512  # Number of nodes in a dense layer
+MEMORY = SequentialMemory(
+    limit=MAX_EXPERIENCES,
+    window_length=WINDOW_LENGTH
+)
 
-DENSE = 512
-
-
+# Overwritten class to turn an observation (screenshot) into a resized greyscale image. Also clips the reward to [-1, 1]
 class ImageProcessor(Processor):
     def process_observation(self, obs):
-
         processed_obs = Image.fromarray(obs)
 
         processed_obs = ImageOps.pad(  # resize and fit to aspect ratio by padding
@@ -53,92 +56,124 @@ class ImageProcessor(Processor):
         return processed_batch
 
     def process_reward(self, reward):
-        return np.clip(reward, -1., 1.)
+        return np.clip(reward, -1, 1)
 
 
+# Creates a neural network give the number of desired outputs k
 def make_model(k):
     model = Sequential()
     model.add(Permute(
         (2, 3, 1),
         input_shape=(WINDOW_LENGTH,) + TARGET_IMAGE_SHAPE
     ))
-    # self.model.add(ConvLSTM2D())
     model.add(Conv2D(
-        128,  # Filters i.e. outputs
-        (32, 32),  # Kernal size i.e. size of window inside nn
+        32,  # Filters i.e. outputs
+        (16, 16),  # Kernal size i.e. size of window inside nn
         strides=(4, 4),
-        activation='relu',
-        kernel_initializer='RandomUniform'
+        activation='relu'
     ))
     model.add(Conv2D(
         64,
-        (16, 16),
+        (8, 8),
         strides=(2, 2),
-        activation='relu',
-        kernel_initializer='RandomUniform'
+        activation='relu'
     ))
     model.add(Conv2D(
-        64,
-        (1, 1),
+        128,
+        (4, 4),
         strides=(1, 1),
-        activation='relu',
-        kernel_initializer='RandomUniform'
+        activation='relu'
     ))
     model.add(Flatten())
     model.add(Dense(
         units=DENSE,
-        activation='relu',
-        kernel_initializer='RandomUniform'
+        activation='relu'
     ))
     model.add(Dense(
         units=k,
-        activation='linear',
-        kernel_initializer='RandomUniform'
+        activation='linear'
     ))
 
     return model
 
 
-def make_DQN_agent(k, train, training_actions=None):
+# Makes a Deep
+def make_DQN_model(k, ddqn, dueling):
     model = make_model(k)
-
-    memory = SequentialMemory(
-        limit=MAX_EXPERIENCES,
-        window_length=WINDOW_LENGTH
-    )
 
     processor = ImageProcessor()
 
-    if train:
-        policy = LinearAnnealedPolicy(
-            inner_policy=EpsGreedyQPolicy(),
-            attr='eps',
-            value_max=EPSILON_MAX,
-            value_min=EPSILON_MIN,
-            value_test=EPSILON_TEST,
-            nb_steps=MAX_EXPERIENCES
-        )
-    else:
-        policy = GreedyQPolicy()
+    policy = LinearAnnealedPolicy(
+        inner_policy=EpsGreedyQPolicy(),
+        attr='eps',
+        value_max=EPSILON_MAX,
+        value_min=EPSILON_MIN,
+        value_test=EPSILON_TEST,
+        nb_steps=STEPS
+    )
 
     agent = DQNAgent(
         nb_actions=k,
         model=model,
-        memory=memory,
+        memory=MEMORY,
         processor=processor,
         policy=policy,
         nb_steps_warmup=EXAMPLE_PERIOD,
         gamma=GAMMA,
         target_model_update=TARGET_UPDATE,
-        train_interval=4,
-        delta_clip=1.0,
-        training_actions=training_actions
+        train_interval=WINDOW_LENGTH,
+        enable_double_dqn=ddqn,
+        enable_dueling_network=dueling
     )
 
     agent.compile(
-        optimizer=nadam(lr=ALPHA),
-        metrics=['mae']
+        optimizer=Adam(lr=ALPHA)
     )
 
     return agent
 
+
+def train_DQN_model(env, load_file=None, ddqn=False, dueling=False):
+
+    if ddqn and dueling:
+        network_type = 'Dueling_DDQN'
+    elif ddqn:
+        network_type = 'DDQN'
+    elif dueling:
+        network_type = 'Dueling_DQN'
+    else:
+        network_type = 'DQN'
+
+    start_time      = datetime.datetime.now().strftime("%m-%d_%H:%M")
+    weights_file    = f'./weights/{network_type}/{env.gamename}_weights_{start_time}.h5f'
+    log_file        = f'./logs/{network_type}/{env.gamename}_log_{start_time}.json'
+    plot_file       = f'./logs/{network_type}/{env.gamename}_training_data_{start_time}.png'
+
+    agent = make_DQN_model(env.action_space.n, ddqn, dueling)
+
+    if load_file is not None:
+        agent.load_weights(load_file)
+
+    agent.fit(
+        env=env,
+        nb_steps=STEPS,
+        action_repetition=1,
+        callbacks=[FileLogger(log_file)],
+        visualize=False,
+        verbose=2,
+        nb_max_start_steps=10
+    )
+
+    agent.save_weights(
+        weights_file=weights_file,
+        overwrite=True
+    )
+
+    log = pd.read_json(log_file)
+    plt.grid()
+    log.plot(
+        kind='scatter',
+        x='episode',
+        y='episode_reward',
+        title='Reward per episode')
+    plt.savefig(plot_file)
--- Project Files/Main.py
+++ Project Files/Main.py
@@ -1,54 +1,44 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
 import Deep_Q_Learning_Keras as DQL
-from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 import retro
+import retrowrapper
+from rl.callbacks import FileLogger
+from stable_baselines import PPO2
+from stable_baselines.common import make_vec_env
 
 # Constants
-ENVIRONMENT             = 'Galaga-Nes'
-WEIGHTS_FILE            = f'dqn_{ENVIRONMENT}_weights.h5f'
-CHECKPOINT_WEIGHTS_FILE = f'dqn_{ENVIRONMENT}_weights_checkpoint.h5f'
-
-TRAIN   = True
-LOAD    = False
-RECORD  = True
+ENVIRONMENT = 'Galaga-Nes'
+PARALLEL_GAMES = 64
 
 if __name__ == '__main__':
-
-    env = retro.RetroEnv(
+    env = retro.make(
         game=ENVIRONMENT,
         use_restricted_actions=retro.Actions.DISCRETE,
-        record=RECORD,
+        record=True,
         inttype=retro.data.Integrations.ALL
     )
-
     env.reset()
+    DQL.train_DQN_model(env)
+    # DQL.train_DQN_model(env, ddqn=True)
+    # DQL.train_DQN_model(env, dueling=True)
+    # DQL.train_DQN_model(env, ddqn=True, dueling=True)
 
-    agent = DQL.make_DQN_agent(env.action_space.n, TRAIN)
-
-    callbacks = [ModelIntervalCheckpoint(CHECKPOINT_WEIGHTS_FILE, interval=50000)]
-
-    if TRAIN:
-        if LOAD:
-            agent.load_weights(WEIGHTS_FILE)
-
-        agent.fit(
-            env=env,
-            nb_steps=2500000,
-            action_repetition=1,
-            callbacks=callbacks,
-            visualize=False,
-            log_interval=DQL.TARGET_UPDATE,
-        )
-
-        agent.save_weights(WEIGHTS_FILE, overwrite=True)
-
-    else:
-        agent.load_weights(WEIGHTS_FILE)
-        agent.test(
-            env,
-            nb_episodes=1,
-            visualize=True
-        )
-
-
-
-
+    # env = make_vec_env(
+    #     env_id='Galaga-Nes',
+    #     n_envs=PARALLEL_GAMES
+    # )
+    #
+    # model = PPO2(
+    #     policy='CnnLstmPolicy',
+    #     env=env,
+    #     gamma=DQL.GAMMA,
+    #     learning_rate=DQL.ALPHA,
+    #     nminibatches=PARALLEL_GAMES,
+    #     verbose=1,
+    #     prioritized_replay=True
+    # )
+    # model.learn(total_timesteps=DQL.STEPS)
+    # model.save(f'./weights/PPO/{ENVIRONMENT}_model')
